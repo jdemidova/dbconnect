9d4696212d0dc8a715a87cfdea9b507494bd6da1
jdemidova <demidova.j.o.98@gmail.com>
2019-07-07 11:24:00 +0400

    v2

--- src/main/java/com/practice/dbconnect/Application.java
+++ src/main/java/com/practice/dbconnect/Application.java
@@ -1,32 +1,36 @@
 package com.practice.dbconnect;
 
-import com.practice.dbconnect.repos.EmployeesRep;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+//
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+//public class Application implements CommandLineRunner {
+//
+//    @Autowired
+//    private StartController startController;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... strings) throws Exception {
+//        System.out.println(startController);
+//    }
+//}
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private StartController startController;
+@SpringBootApplication
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-        System.out.println(startController);
-    }
 }
--- src/main/java/com/practice/dbconnect/repos/EmployeesRep.java
+++ src/main/java/com/practice/dbconnect/repos/EmployeesRep.java
@@ -1,11 +1,9 @@
 package com.practice.dbconnect.repos;
 
-import com.practice.dbconnect.entities.Employees;
-import org.springframework.context.annotation.Bean;
+import com.practice.dbconnect.entities.EmployeeBean;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 //@Repository
-public interface EmployeesRep extends CrudRepository<Employees, Integer> {
+public interface EmployeesRep extends CrudRepository<EmployeeBean, Integer> {
+
 }
